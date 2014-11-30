73a5f6e7b5a9961cf42cc8e6e629023d32d70b58
Adway Dhillon <adhillon6@gatech.edu>
2014-11-30 04:21:23 -0500

    Alternating Characters

--- alternatingCharacters.java
+++ alternatingCharacters.java
@@ -1,3 +1,14 @@
+/* Problem Statement
+*
+* Shashank likes strings in which consecutive characters are different. 
+* For example, he likes ABABA, while he doesn't like ABAA. 
+* Given a string containing characters A and B only, he wants to change it into a string he likes. 
+* To do this, he is allowed to delete the characters in the string.
+*
+* Your task is to find the minimum number of required deletions.
+*/
+
+
 import java.util.*;
 
 public class alternatingCharacters {
